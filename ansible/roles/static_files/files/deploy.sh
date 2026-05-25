cd08a6f0237a509719939ca090662edb08bf6cb9
steve <pub@davisgroup.uk>
2026-05-25 16:47:43 -0500

    1779745663

--- ansible/roles/static_files/files/deploy.sh
+++ ansible/roles/static_files/files/deploy.sh
@@ -3,5 +3,3 @@ rsync --info=NAME -r --delete /var/www/html-predeploy/ /var/www/html/
 rsync --info=NAME -r --delete /var/www/secret-predeploy/ /var/www/secret/
 chown -Rv root:root /var/www/html/
 find /var/www/html -mindepth 1 -exec restorecon -v {} \;
-find /var/www/html /var/www/html-predeploy /var/www/secret /var/www/secret-predeploy -type f -mtime +30 -delete
-find /var/www/html /var/www/html-predeploy /var/www/secret /var/www/secret-predeploy -type d -empty -delete
