5ca4bc5a1ff9c9cf9de6021714de6d6056dac453
steve <pub@davisgroup.uk>
2026-06-27 14:52:33 -0500

    Work

---
+++ ansible/roles/deploy_website/files/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo -u deploy bash -c "cd /home/deploy/git/davisgroup.uk && bun upgrade && uv self update && uv run make.py build"
+rsync -rv --delete /home/deploy/git/davisgroup.uk/dist/ /var/www/html/
+rsync -rv --delete /var/www/secret-predeploy/ /var/www/secret/
+chown -Rv root:root /var/www/html/
+find /var/www/html -mindepth 1 -exec restorecon -v {} \;
