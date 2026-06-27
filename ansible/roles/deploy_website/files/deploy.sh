9cc20ff417851fc425e6ff51b0598119a4b06d6b
steve <pub@davisgroup.uk>
2026-06-27 18:33:31 -0500

    1782603211

--- ansible/roles/deploy_website/files/deploy.sh
+++ ansible/roles/deploy_website/files/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo -u deploy bash -c "cd /home/deploy/git/davisgroup.uk && bun upgrade && uv self update && git pull origin main && uv run make.py build"
+set -e
+sudo -u deploy bash -c 'export PATH="$PATH:/home/deploy/.local/bin" && cd /home/deploy/git/davisgroup.uk && bun upgrade && uv self update && git pull origin main && uv run make.py build'
 rsync -rv --delete /home/deploy/git/davisgroup.uk/dist/ /var/www/html/
 rsync -rv --delete /var/www/secret-predeploy/ /var/www/secret/
 chown -Rv root:root /var/www/html/
